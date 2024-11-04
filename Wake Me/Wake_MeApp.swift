e6dee3c647f8c278d9211c5d7af063e00049f30b
h-ch22 <ckdwls9460@gmail.com>
2024-11-04 10:12:31 +0900

    Initial Commit

---
+++ Wake Me/Wake_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wake_MeApp.swift
+//  Wake Me
+//
+//  Created by Changjin Ha on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Wake_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wake MeTests/Wake_MeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Wake_MeTests.swift
+//  Wake MeTests
+//
+//  Created by Changjin Ha on 11/4/24.
+//
+
+import Testing
+
+struct Wake_MeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
